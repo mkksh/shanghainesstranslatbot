68732e3d73b5850e24bd9059c6f989e44509d09b
Mike <oashsj@gmail.com>
2020-04-19 22:17:00 -0500

    Update webhookmsgtest - git.php
    update for display the username when bot recive the info from a user in a gorup. it  will display like "username@groupname:"

--- commuicatetest/webhookmsgtest - git.php
+++ commuicatetest/webhookmsgtest - git.php
@@ -1,16 +1,26 @@
 <?php
 
-$BotToken="88888";
+$BotToken="your token";
 $WebAddress="https://api.telegram.org/bot".$BotToken;
 $updates=file_get_contents("php://input");
 
 $updatearray=json_decode($updates, TRUE);
-//$msgtest=$updatearray["result"][0]["update_id"];
+if (!$updatearray) {
+    // receive wrong update, must not happen
+    exit;
+  }
+
+  if (isset($updatearray["message"])) {
+    
+  
+  //$msgtest=$updatearray["result"][0]["update_id"];
 $msgchatid=$updatearray["message"]["chat"]["id"];
+$msgfromusername=$updatearray["message"]["from"]["username"];
 $msgusername=$updatearray["message"]["chat"]["username"];
 $msgtext=$updatearray["message"]["text"];
 
+/
+file_get_contents($WebAddress."/sendmessage?chat_id=".$msgchatid."&text=".$msgfromusername."@".$msgusername.":".$msgtext);
 
-file_get_contents($WebAddress."/sendmessage?chat_id=".$msgchatid."&text=".$msgusername.":".$msgtext);
 
 ?>
